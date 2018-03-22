d6718971a8fe0740e73a1d69ee9631e44ffdd93b
Fraser Price <fraserbcprice@gmail.com>
2018-03-22 08:20:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import hc12
 from nxp_imu import IMU
-import bme280
+import bme680
 
+sensor = bme680.BME680()
 imu = IMU(gs=4, dps=2000, verbose=True)
 
 while True:
-        pres,temp,hum=bme280.readAll()
-        final="B:" + str(pres) + "," + str(temp)  + "," + str(hum) + ";"
+        if sensor.get_sensor_data():
+            output = str(sensor.data.temperature) + "," + str(sensor.data.pressure) + "," + str(sensor.data.humidity) + "," + str(sensor.data.gas_resistance)
+        final="B:" + output + ";"
         a, m, g=imu.get()
         final=final + "D:" + str(a) + "," + str(m) + "," + str(g) + ";"
         hc12.send(final)
