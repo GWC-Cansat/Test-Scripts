a99c97faaade0752cce3e5b3d342d448156051aa
GWC CanSat <33035543+gwccansat@users.noreply.github.com>
2019-03-27 18:41:27 +0000

    Delete Luncher.java

--- gui/src/cansat/Luncher.java
+++
@@ -1,9 +0,0 @@
-package cansat;
-
-import javafx.application.Application;
-
-public class Luncher {
-   public static void main(String[] args) {
-      Application.launch(MainLoop.class, args);
-   }
-}
